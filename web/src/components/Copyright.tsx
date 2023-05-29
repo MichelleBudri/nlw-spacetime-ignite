235c43c28739f39bb2c0ad33920602e98307bf5d
Michelle Budri Bognar <michellebudri@yahoo.com>
2023-05-29 18:41:48 -0300

    WEB - Separando o front-end em componentes

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,7 @@
+export function Copyright(){
+    return(
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da <a href="https://rocketseat.com.br" rel="noreferrer" target="_blank" className="underline hover:text-gray-100">Rocketseat</a>
+        </div>
+    )
+}
